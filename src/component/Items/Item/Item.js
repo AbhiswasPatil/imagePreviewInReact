7a2e0956740a4ae740609886cf679193727f5aed
Abhishek Patil <abhiswaspatil1710@gmail.com>
2021-06-25 10:34:14 +0530

    truncated titleText

--- src/component/Items/Item/Item.js
+++ src/component/Items/Item/Item.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Item.css";
-const item = (props) => {
-  return (
-    <div className={props.class} onClick={props.clicked}>
-      <img alt={props.title} src={props.previewImage} />
-      <div className="info">{props.title}</div>
-    </div>
-  );
-};
+class item extends Component {
+  constructor(props) {
+    super(props);
+    this.titleText = React.createRef();
+  }
+  truncateTitleText(el) {
+    if (el.scrollWidth > el.clientWidth) {
+      var textData = el.textContent;
+      var estimatedChars = Math.floor(
+        (el.clientWidth * textData.length) / el.scrollWidth
+      );
+      var estimatedLength = Math.floor(estimatedChars / 2) - 3;
+      var newTextData =
+        textData.slice(0, 0 + estimatedLength) +
+        "..." +
+        textData.slice(textData.length - 1 - estimatedLength, textData.length);
+      el.textContent = newTextData;
+    }
+  }
+  componentDidMount() {
+    console.log(
+      this.titleText.current.scrollWidth,
+      this.titleText.current.clientWidth
+    );
+    this.truncateTitleText(this.titleText.current);
+  }
+  render() {
+    return (
+      <div className={this.props.class} onClick={this.props.clicked}>
+        <img alt={this.props.title} src={this.props.previewImage} />
+        <div className="info" ref={this.titleText}>
+          {this.props.title}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default item;
